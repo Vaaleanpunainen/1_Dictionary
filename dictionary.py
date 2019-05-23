6d74fa9837ee6ed09a340a2b5d14265aa3cacef4
Ewelina Backtrog <oyfg@o2.pl>
2019-05-23 10:17:24 +0200

    update code of user misspells input

--- dictionary.py
+++ dictionary.py
@@ -1,13 +1,23 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(w):
-
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = (input("Maybe you mean %s instead? Enter Y for yes or N for no: " % get_close_matches(w, data.keys())[0])).upper()
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "No such a word in a dictionary. Please double check it."
+        else:
+            return "Incorrect enter."
     else:
         return "No such a word in a dictionary. Please double check it."
 word = (input("Enter world: ")).lower()
+
+
 
 print(translate(word))
