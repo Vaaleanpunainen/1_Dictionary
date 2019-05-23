c585e69febccc0fe97dd6fbd31ddec05e1d95f6e
Ewelina Backtrog <oyfg@o2.pl>
2019-05-23 09:18:20 +0200

    update code for case sensitivity

--- dictionary.py
+++ dictionary.py
@@ -3,10 +3,11 @@ import json
 data = json.load(open("data.json"))
 
 def translate(w):
+
     if w in data:
         return data[w]
     else:
         return "No such a word in a dictionary. Please double check it."
-word = input("Enter world: ")
+word = (input("Enter world: ")).lower()
 
 print(translate(word))
